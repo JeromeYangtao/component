f7f7139c9903f8ba73429511f702220e1bc3283c
Thomson <779387850@qq.com>
2018-04-28 12:29:51 +0800

    fix & test

--- LRU-storage插件/lru-storage.js
+++ LRU-storage插件/lru-storage.js
@@ -3,7 +3,7 @@
  * localStorage是以域名为单元进行存储，那么在一个域名下，就会有存储key重复的风险，可能导致页面出错。
  * 浏览器下容量基本限制在 5M 左右。尽管已经很大了，但是当超出存储容量时，js会抛出DOM exception，可能导致程序错误。
  * 相比cookie, Storage没有数据过期时间的概念, 一些已经不用的数据一直存在用户的机器上，更容易导致问题1的出现。
- * 
+ *
  * 对应解决方案:
  * key值前加一个每个人独有的前缀，默认添加一个时间的hash值
  * LRU(Least Recently Used) 进行缓存淘汰
@@ -13,12 +13,13 @@
 
 let num = 0 //localStorage中的对象数
 
-function LRU() {
+function LRU () {
 
 }
+
 let memQueue = []
-memQueue.prototype.remove = function (val) {
-  var index = this.indexOf(val);
+Array.prototype.remove = function (val) {
+  var index = this.indexOf(val)
   if (index > -1) {
     this.splice(index, 1)
   }
@@ -28,7 +29,7 @@ memQueue.prototype.remove = function (val) {
  * @param prefix  前缀
  * @param config  配置项，缓存过期时间maxAge，最大使用数limit
  */
-function lruStorage(prefix, config) {
+function lruStorage (prefix, config) {
   if (window.localStorage) {
     let localStorage = window.localStorage
   } else {
@@ -36,14 +37,14 @@ function lruStorage(prefix, config) {
   }
 
   prefix = prefix
-  config = {
-    maxAge: config.maxAge || 1000 * 60 * 60 * 24, //一天
-    limit: config.limit || 100
+  config = config ? config : {
+    maxAge: 1000 * 60 * 60 * 24, //一天
+    limit: 100
   }
 
   return {
     set: function (key, value) {
-      key = key + '-' + prefix
+      key = prefix + '-' + key
       if (num < config.limit) {
         value._time = Date.now()
         JSON.stringify(value)
@@ -54,17 +55,17 @@ function lruStorage(prefix, config) {
         localStorage.setItem(key, value)
         memQueue.unshift(key)
 
-        let = lruObj = memQueue.pop()
+        let lruObj = memQueue.pop()
         localStorage.removeItem(lruObj)
       }
     },
     get: function (key) {
-      key = key + '-' + prefix
+      key = prefix + '-' + key
       let now = Date.now()
       let value = localStorage.getItem(key)
       value = JSON.parse(value)
 
-      if (now - value._time > config.maxAge) {
+      if (!value || now - value._time > config.maxAge) {
         localStorage.removeItem(key)
         num--
         memQueue.remove(key)
@@ -78,28 +79,6 @@ function lruStorage(prefix, config) {
   }
 }
 
-
-export default {
-  lruStorage
-}
-
-// 用户使用
-// lru = lruStorage('test'); //data will be storaged as 'test-xxx' in webStorage
-// lru.set('foo', 1);
-// lru.get('foo');  //1
-// lru.get('bar');  //undefined
-
-// lru = LruWebStorage('testStale', {maxAge: 6 * 1000});
-// lru.set('a', 1);
-// lru.get('a');    //null
-
-
-// lru = LruWebStorage('testLimit', {limit: 2});
-// lru.set('a', 1);
-// lru.set('b', 2);
-// lru.get('a');
-// lru.set('c', 3);
-
-// lru.get('c');    //3
-// lru.get('b');    //undefined
-// lru.get('a');    //1
+// export default {
+//   lruStorage
+// }
